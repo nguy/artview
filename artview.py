4bd435f934923ff12c03ff10fe71fb56bf5522c6
Paul Hein <hein@atmos.colostate.edu>
2015-06-18 17:28:43 -0600

    Fixed onPick 360 deg problem

--- artview.py
+++ artview.py
@@ -210,6 +210,13 @@ class Browse(QtGui.QMainWindow):
             az = az + 360.
         rng = np.sqrt(xdata*xdata+ydata*ydata)
         azindex = np.argmin(np.abs(self.radar.azimuth['data'][self.radar.sweep_start_ray_index['data'][self.tilt]:self.radar.sweep_end_ray_index['data'][self.tilt]+1]-az))+self.radar.sweep_start_ray_index['data'][self.tilt]
+        if azindex == self.radar.sweep_end_ray_index['data'][self.tilt]:
+            if az < 10:
+                az = az + 360.
+            if np.abs(self.radar.azimuth['data'][self.radar.sweep_start_ray_index['data'][self.tilt]]+360.-az) < np.abs(self.radar.azimuth['data'][self.radar.sweep_end_ray_index['data'][self.tilt]]-az):
+                azindex = self.radar.sweep_start_ray_index['data'][self.tilt]
+            else:
+                azindex = self.radar.sweep_end_ray_index['data'][self.tilt]
         rngindex = np.argmin(np.abs(self.radar.range['data']-rng*1000.))
         msg = 'x = %4.2f, y = %4.2f, Azimuth = %4.2f deg., Range = %4.3f km, %s = %4.2f %s'\
         %(xdata, ydata, self.radar.azimuth['data'][azindex], self.radar.range['data'][rngindex]/1000., self.field, self.radar.fields[self.field]['data'][azindex][rngindex], self.units)
