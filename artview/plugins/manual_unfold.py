4e63ed6d61a52b7b0a1461f82a00d39286e18e58
Gama Anderson <gamaanderson92@gmail.com>
2015-09-25 20:38:25 +0200

    add manual unfold plugin and script

---
+++ artview/plugins/manual_unfold.py
@@ -0,0 +1,176 @@
+"""
+manual_unfold.py
+"""
+
+# Load the needed packages
+import code
+import pyart
+
+import sys
+import os
+import numpy as np
+
+path = os.path.dirname(sys.modules[__name__].__file__)
+path = os.path.join(path, '...')
+sys.path.insert(0, path)
+
+import artview
+
+from ..core import Component, Variable, common, QtGui, QtCore, componentsList
+
+
+class ManualUnfold(Component):
+    '''
+    Use of Points to Unfold Velocity in Radar
+    '''
+
+    Vradar = None  #: see :ref:`shared_variable`
+    Vpoints = None  #: see :ref:`shared_variable`
+
+    @classmethod
+    def guiStart(self, parent=None):
+        '''Graphical interface for starting this class.'''
+        kwargs, independent = \
+            common._SimplePluginStart("ManualUnfold").startDisplay()
+        kwargs['parent'] = parent
+        return self(**kwargs), independent
+
+    def __init__(self, Vradar=None, Vpoints=None, name=" ManualUnfold",
+                 parent=None):
+        '''Initialize the class to create the interface.
+
+        Parameters
+        ----------
+        [Optional]
+        Vradar : :py:class:`~artview.core.core.Variable` instance
+            Radar signal variable.
+            A value of None will instantiate a empty variable.
+        Vpoints : :py:class:`~artview.core.core.Variable` instance
+            Points signal variable.
+            A value of None will instantiate a empty variable.
+        name : string
+            Field Radiobutton window name.
+        parent : PyQt instance
+            Parent instance to associate to this class.
+            If None, then Qt owns, otherwise associated with parent PyQt
+            instance.
+        '''
+        super(ManualUnfold, self).__init__(name=name, parent=parent)
+
+        if Vradar is None:
+            self.Vradar = Variable(None)
+        else:
+            self.Vradar = Vradar
+
+        if Vpoints is None:
+            self.Vpoints = Variable(None)
+        else:
+            self.Vpoints = Vpoints
+
+        self.sharedVariables = {"Vradar": self.newRadar,
+                                "Vpoints": None}
+        self.connectAllVariables()
+
+        self.central_widget = QtGui.QWidget()
+        self.setCentralWidget(self.central_widget)
+        self.layout = QtGui.QGridLayout(self.central_widget)
+
+        self.velField = QtGui.QLineEdit(
+            pyart.config.get_field_name('velocity'))
+        self.layout.addWidget(QtGui.QLabel("vel_field"), 0, 0)
+        self.layout.addWidget(self.velField, 0, 1)
+
+        self.corrVelField = QtGui.QLineEdit(
+            pyart.config.get_field_name('corrected_velocity'))
+        self.layout.addWidget(QtGui.QLabel("corr_vel_field"), 1, 0)
+        self.layout.addWidget(self.corrVelField, 1, 1)
+
+        self.nyquistVelocity = QtGui.QDoubleSpinBox()
+        self.nyquistVelocity.setRange(-1, 1000)
+        self.nyquistVelocity.setValue(-1)
+        self.layout.addWidget(QtGui.QLabel("nyquist_velocity"), 2, 0)
+        self.layout.addWidget(self.nyquistVelocity, 2, 1)
+
+        self.positiveButton = QtGui.QPushButton("Unfold Positive Values")
+        self.positiveButton.clicked.connect(self.positiveUnfold)
+        self.layout.addWidget(self.positiveButton, 3, 1)
+
+        self.negativeButton = QtGui.QPushButton("Unfold Negative Values")
+        self.negativeButton.clicked.connect(self.negativeUnfold)
+        self.layout.addWidget(self.negativeButton, 4, 1)
+
+        self.show()
+
+    def getFieldNames(self):
+        vel = str(self.velField.text())
+        if vel == '':
+            vel = pyart.config.get_field_name('velocity')
+        if vel not in self.Vradar.value.fields:
+            common.ShowWarning(
+                "vel_field not in Radar, can not perform correction")
+
+        corrVel = str(self.corrVelField.text())
+        if corrVel == '':
+            corrVel = pyart.config.get_field_name('corrected_velocity')
+
+        return vel, corrVel
+
+    def positiveUnfold(self):
+        self.unfold('positive')
+
+    def negativeUnfold(self):
+        self.unfold('negative')
+
+    def unfold(self, side):
+        # test radar
+        radar = self.Vradar.value
+        points = self.Vpoints.value
+        if radar is None:
+            common.ShowWarning("Radar is None, can not perform correction")
+            return
+
+        if points is None:
+            common.ShowWarning("Points is None, can not perform correction")
+            return
+
+        if ('ray_index' not in points.axes or
+            'range_index' not in points.axes):
+            common.ShowWarning(
+                "Error: Points must have axes 'ray_index' and 'range_index'")
+            return
+
+        vel, corrVel = self.getFieldNames()
+        data = radar.fields[vel]['data'].copy()
+        ray = points.axes['ray_index']['data']
+        rng = points.axes['range_index']['data']
+        nyquist = self.nyquistVelocity.value()
+        if side == 'positive':
+            data[ray,rng] = np.where(data[ray,rng]>0,
+                                     -2*nyquist+data[ray,rng], data[ray,rng])
+        elif side == 'negative':
+            data[ray,rng] = np.where(data[ray,rng]<0,
+                                     2*nyquist+data[ray,rng], data[ray,rng])
+
+        strong_update = False  # insertion is weak, overwrite strong
+        if corrVel in self.Vradar.value.fields.keys():
+            resp = common.ShowQuestion(
+                "Field %s already exists! Do you want to over write it?" %
+                corrVel)
+            if resp != QtGui.QMessageBox.Ok:
+                return
+            else:
+                strong_update = True
+
+        radar.add_field_like(vel, corrVel, data, replace_existing=True)
+        self.Vradar.update(strong_update)
+
+    def newRadar(self, variable, value, strong):
+        '''respond to change in radar.'''
+        if self.Vradar.value is None:
+            return
+
+        nyquist_vel = self.Vradar.value.get_nyquist_vel(0, True)
+        self.nyquistVelocit.setValue(nyquist_vel)
+
+
+_plugins = [ManualUnfold]
---
+++ artview/scripts/manual_unfold.py
@@ -0,0 +1,76 @@
+"""
+compare_fields.py
+
+Driver function that creates ARTView display.
+"""
+import os
+import sys
+
+from ..core import Variable, QtGui, QtCore
+from ..components import RadarDisplay, Menu, LinkPlugins, SelectRegion
+from ._common import _add_all_advanced_tools, _parse_dir, _parse_field
+from ..plugins import ManualUnfold
+
+def run(DirIn=None, filename=None, field=None):
+    """
+    artview execution for manual unfolding velocity field of radar
+    """
+    DirIn = _parse_dir(DirIn)
+
+    app = QtGui.QApplication(sys.argv)
+
+    # start Menu and initiate Vradar
+    menu = Menu(DirIn, filename, mode="Radar", name="Menu")
+    Vradar = menu.Vradar
+
+    field = _parse_field(Vradar.value, field)
+
+    # start Displays
+    Vtilt = Variable(0)
+    plot1 = RadarDisplay(Vradar, Variable(field), Vtilt, name="Display1",
+                         parent=menu)
+    plot2 = RadarDisplay(Vradar, Variable(field), Vtilt, name="Display2",
+                         parent=menu)
+
+    # connect zoom
+    plot2.disconnectSharedVariable('Vlims')
+    plot2.Vlims = plot1.Vlims
+    plot2.connectSharedVariable('Vlims')
+
+    # start region selection tool
+    roi = SelectRegion(plot1, name="SelectRegion", parent=menu)
+
+    # start unfold tool
+    unfold = ManualUnfold(Vradar, roi.Vpoints, name=" ManualUnfold",
+                          parent=menu)
+
+    # start ComponentsControl
+    control = LinkPlugins()
+
+    # add components to Menu
+    menu.addLayoutWidget(control)
+    menu.addLayoutWidget(roi)
+    menu.addLayoutWidget(unfold)
+    control.show()
+    roi.show()
+    unfold.show()
+    # add grafical starts
+    _add_all_advanced_tools(menu)
+
+    # Replace in Screen
+    desktop_rect = QtGui.QDesktopWidget().screenGeometry()
+
+    height = desktop_rect.height()
+    width = desktop_rect.width()
+
+    menu_width = 300
+    menu_height = 180
+
+    menu.setGeometry(0, 0, menu_width, menu_height)
+
+    plot_size = min(height-60-menu_height, width/2) - 50
+    plot1.setGeometry(0, height-plot_size, plot_size, plot_size)
+    plot2.setGeometry(width/2, height-plot_size, plot_size, plot_size)
+
+    # start program
+    app.exec_()
