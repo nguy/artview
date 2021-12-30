6b1777c94855d69f96e33981205f1d454a9b3c89
Eugene Savelov <savelov@gmail.com>
2021-12-30 20:49:50 +0300

    fix Qt5 radar show display

--- artview/components/plot_radar.py
+++ artview/components/plot_radar.py
@@ -12,7 +12,7 @@ import pyart
 from matplotlib.backends.qt_compat import is_pyqt5
 if is_pyqt5():
     from matplotlib.backends.backend_qt5agg import (
-        FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
+        FigureCanvas, NavigationToolbar2QT as NavigationToolbar, FigureCanvasQTAgg)
 else:
     from matplotlib.backends.backend_qt4agg import (
         FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
